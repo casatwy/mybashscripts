ba45767e7349b35e68e8f0e29de430efe80354d7
casa <tianwieyu@shizhuang-inc.com>
2021-01-13 13:34:50 +0800

    modification

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 cp ./PushPrivateRepo /usr/local/bin
 cp ./RemoveGitHooks /usr/local/bin
-+cp ./RemoveGitHooks /usr/local/bin
-+cp ./RemoveGitHooks /usr/local/bin
-+cp ./RemoveGitHooks /usr/local/bin
-+cp ./RemoveGitHooks /usr/local/bin
